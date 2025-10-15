5717d3bcb550885169746f0a6e6c046a5e6e80aa
VitaPC <vitek.f18@gmail.com>
2025-10-15 18:35:08 +0200

    všichni přidaný & naše věty

--- script.js
+++ script.js
@@ -94,33 +94,47 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const students = [
+// Students data for IT4B
+const IT4Bstudents = [
+  { name: "Daniel Bock", img: "images/DSC_0378.jpg", text: "Danovo nejoblíbenější aktivita je spánek. Rád si zahraje s kámošema videohry. Radši tráví čas doma než venku. Poslouchá muziku 24/7." },
+  { name: "Borislav Boyanov", img: "images/DSC_0155.jpg", text: "Borislav rád sází, zbožňuje hrát a koukat na sporty. Miluje řídit auta. Nonstop pracuje a baví ho vydělávat peníze." },
+  { name: "Martin Brunclík", img: "images/DSC_0197.jpg", text: "Jeho vevodská výsost Martin Brunclík brunšvický, veleprogramátor plzeňský a arcisysadmin třemošenský jest člověk srdečně zapálený technokrat se sklony k (často) nežádoucímu šíření zbytečných informací a faktoidů. Možno též říci: celkem fajn člověk." },
+  { name: "Gabriel Čonka", img: "images/DSC_0115.jpg", text: "Gabriel je kamarádský, sympatický a hodně milý hoch. Gabriel se rád prochází a potkáva nové lidi. Má hrozně specifický smysl pro humor." },
+  { name: "Roman Damianenko", img: "images/DSC_0106.jpg", text: "Roman je docela slušný a klidný kluk. Ve volném čase rád poslouchá různou hudbu. Taky si občas rád trošku zacvičí." },
+  { name: "Marat Fedorov", img: "images/DSC_0108.jpg", text: "Marat Fedorov má rád vytváření webových stránek. Většinu času dělá na své maturitní stránce nebo je se svojí polovičkou. A jako každý nesnáší pondělní rána." },
+  { name: "Vítek Fikrle", img: "images/DSC_0447.jpg", text: "Vítek je kluk, který tráví nezdravé množství času (a peněz) hraním videoher. Je blázen do technologie, volný čas tráví sledováním novinek a sestavou nebo konfigurací počítačů." },
+  { name: "Ivan Gemela", img: "images/DSC_0230.jpg", text: "Sundal by z ulice každou kameru. Přál by si aby lidi víc mysleli a míň mluvili. Co je nahoře domovní bratře? No tak baby nestyď se ukaž mu tu žábu." },
+  { name: "Daniel Holý", img: "images/DSC_0180.jpg", text: "Dan je profesionální prokrastinátor, bez jediný motivace k dokončení studia." },
+  { name: "Alexander Kavnjanyk", img: "images/DSC_0272.jpg", text: "Saša je fajnšmekr piva. Jeho nejoblíbenější aktivita je grilování. Má rád sekanou. Nejradši tráví čas s kamarády a spolužáky." },
+  { name: "Jan Kirpal", img: "images/DSC_0246.jpg", text: "Honza je sleeperbuild master. Vůbec se nezdá ale svali má. Dokáže zaspat i na akci v 17:00. Má neprůstřelné argumenty a vždy má pravdu." },
+  { name: "Jakub Kořínský", img: "images/DSC_0345.jpg", text: "Jeho hraběcí excelence Jakub Kořínský z Kohořína jest poněkud zamlklý intelektuál se zálibami v mnoha oblastech. Rád poslouchá vážnou hudbu a nepohrdne ani historickými, politickými či matematickými fakty. Pohrda menšinami a lidskou spodinou." },
+  { name: "Daniel Koukolík", img: "images/DSC_0236.jpg", text: "Daniel je profesionální uživatel tabáku. Jeho hlavní zálibou posilování. Má větší bicák než ty hlavu. Je také hodný a zdvořilý. Rád papá hodně jídla." },
+  { name: "David Lavička", img: "images/DSC_0140.jpg", text: "David má v oblibě auta, líbi se mu zejména jejich design. Kromě toho rád sportuje, přičemž jeho nejoblíbenějším sportem je fotbal. Ve svém volném čase ho baví sledovat různe filmy nebo seriály." },
+  { name: "Adam Lavrikov", img: "images/DSC_0453.jpg", text: "Adam vás překvapí svou energii a pracovitostí. Nejvíce ho baví tvorba nových projektu, plánovat a organizovat akce, při kterých dokáže skloubit hudbu, zábavu a zároveň práci. Je workoholick a všechno se snaží mít takzvaně 'ŤIP-ŤOP'." },
+  { name: "Nguyen Cong Tien Dung", img: "images/DSC_0323.jpg", text: "On je profesionální hráč ve valorantu/lolku. A každý den pracuje ve večerce neboli 24/7. A je to tallass asian" },
+  { name: "Šimon Opatrný", img: "images/DSC_0303.jpg", text: "Šimon se rád zabývá písničkami. Je to člověk, který v sobě spojuje introvertní i extrovertní rysy. Rád traví čas o samotě, ale i s přáteli. Snaží se být co nejvíce pozitivní a šířit dobrou náladu." },
+  { name: "David Šticha", img: "images/DSC_0361.jpg", text: "David se zajímá o historii, především druhé světové války. Hraje airsoft a rád s přáteli podnikne spontánní aktivity. Dokáže pobavit svými pohotovými poznámkami a humorem." },
+  { name: "Daryna Toporovska", img: "images/DSC_0128.jpg", text: "Dáša je holka, co miluje spánek víc než ranní rozvrhy. Přestávky tráví s kamarádkama a neomluvený hodiny ji dokážou vytočit víc než pondělní budík" },
+  { name: "Vojtěch Vašmucius", img: "images/DSC_0427.jpg", text: "Rád si dá šlofíka kdykoliv kdy může. Velmi rád si zapálí a popije. Rád si zahraje s kamarády videohry." },
+  { name: "Tobiáš Veverka", img: "images/DSC_0287.jpg", text: "Tobiáš rád poslouchá tvrdou muziku jako Korn, Ill Niño nebo Slipknot. Miluje hraní her (je to jeho závislost). Umí si udělat ze všeho srandu (doslova ze všeho)." },
+  { name: "Jakub Vlček", img: "images/DSC_0376.jpg", text: "Jakub rád cestuje a užívá si procházky přírodou. Rád se opije ale vždy jen s Mírou (jeho táta). Nikdo ho v životě neviděl nosit brýle." },
+  { name: "Vaclav Vostatek", img: "images/DSC_0241.jpg", text: "Vašek je cigán prej ale jenom z 1/4 (jedný čtvrtiny) takže gádžovskéj gen převláda. (HEHE). Není mu zima, je zloba, je stále dole. Miluje pouličné kurvy, vočúvaj ho more." },
+  { name: "Daniel Zeman", img: "images/DSC_0199.jpg", text: "Velice rád dělá co nejmíň aktivit co může. K tomu jeho spánkový režim je velice rozbitý. A jeho oblíbená aktivita je hrát videohry." }
+];
+
+// Students data for 4SCB
+const SCBstudents = [
   { name: "Annie Bílá", img: "images/DSC_0415.jpg", text: "Lorem ipsum..." },
-  { name: "Daniel Bock", img: "images/DSC_0378.jpg", text: "Lorem ipsum..." },
-  { name: "Borislav Boyanov", img: "images/DSC_0155.jpg", text: "Lorem ipsum..." },
-  { name: "Martin Brunclík", img: "images/DSC_0197.jpg", text: "Lorem ipsum..." },
   { name: "Natálie Burdová", img: "images/DSC_0218.jpg", text: "Lorem ipsum..." },
-  { name: "Gabriel Čonka", img: "images/DSC_0115.jpg", text: "Lorem ipsum..." },
-  { name: "Marat Fedorov", img: "images/DSC_0108.jpg", text: "Lorem ipsum..." },
-  { name: "Vítek Fikrle", img: "images/DSC_0447.jpg", text: "Lorem ipsum..." },
-  { name: "Ivan Gemela", img: "images/DSC_0230.jpg", text: "Lorem ipsum..." },
   { name: "Julie Heclová", img: "images/DSC_0263.jpg", text: "Lorem ipsum..." },
-  { name: "Kristýna Herejková", img: "images/DSC_0218.jpg", text: "Lorem ipsum..." },
-  { name: "Daniel Holý", img: "images/DSC_0180.jpg", text: "Lorem ipsum..." },
+  { name: "Kristýna Herejková", img: "images/DSC_0218_2.jpg", text: "Lorem ipsum..." },
   { name: "Aneta Hřebíková", img: "images/DSC_0245.jpg", text: "Lorem ipsum..." },
   { name: "Anna Janková", img: "images/DSC_0153.jpg", text: "Lorem ipsum..." },
+  { name: "Vladimíra Jirsová", img: "images/DSC_0109.jpg", text: "Lorem ipsum..." },
   { name: "Hana Kanalošová", img: "images/DSC_0190.jpg", text: "Lorem ipsum..." },
-  { name: "Alexander Kavnjanyk", img: "images/DSC_0272.jpg", text: "Lorem ipsum..." },
-  { name: "Jan Kirpal", img: "images/DSC_0246.jpg", text: "Lorem ipsum..." },
-  { name: "Jakub Kořínský", img: "images/DSC_0345.jpg", text: "Lorem ipsum..." },
-  { name: "Daniel Koukolík", img: "images/DSC_0236.jpg", text: "Lorem ipsum..." },
-  { name: "Adam Lavrikov", img: "images/DSC_0453.jpg", text: "Lorem ipsum..." },
-  { name: "David Lavička", img: "images/DSC_0140.jpg", text: "Lorem ipsum..." },
   { name: "Aneta Mrázová", img: "images/DSC_0286.jpg", text: "Lorem ipsum..." },
   { name: "Eliška Novotná", img: "images/DSC_0268.jpg", text: "Lorem ipsum..." },
-  { name: "Nguyen Cong Tien Dung", img: "images/DSC_0323.jpg", text: "Lorem ipsum..." },
   { name: "Nguyen Quang Truong", img: "images/DSC_0397.jpg", text: "Lorem ipsum..." },
-  { name: "Šimon Opatrný", img: "images/DSC_0303.jpg", text: "Lorem ipsum..." },
   { name: "Linda Pfeiferová", img: "images/DSC_0171.jpg", text: "Lorem ipsum..." },
   { name: "Veronika Pivoňková", img: "images/DSC_0143.jpg", text: "Lorem ipsum..." },
   { name: "Adéla Prskavcová", img: "images/DSC_0210.jpg", text: "Lorem ipsum..." },
@@ -128,51 +142,54 @@ const students = [
   { name: "Natálie Šedivcová", img: "images/DSC_0296.jpg", text: "Lorem ipsum..." },
   { name: "Julie Šambergrová", img: "images/DSC_0178.jpg", text: "Lorem ipsum..." },
   { name: "Eva Štědrá", img: "images/DSC_0260.jpg", text: "Lorem ipsum..." },
-  { name: "David Šticha", img: "images/DSC_0361.jpg", text: "Lorem ipsum..." },
-  { name: "Eva Šváchová", img: "images/DSC_0213.jpg", text: "Lorem ipsum..." },
-  { name: "Daryna Toporovska", img: "images/DSC_0128.jpg", text: "Lorem ipsum..." },
-  { name: "Vojtěch Vašmucius", img: "images/DSC_0427.jpg", text: "Lorem ipsum..." },
-  { name: "Tobiáš Veverka", img: "images/DSC_0287.jpg", text: "Lorem ipsum..." },
-  { name: "Jakub Vlček", img: "images/DSC_0376.jpg", text: "Lorem ipsum..." },
-  { name: "Vaclav Vostatek", img: "images/DSC_0241.jpg", text: "Lorem ipsum..." },
-  { name: "Daniel Zeman", img: "images/DSC_0199.jpg", text: "Lorem ipsum..." }
+  { name: "Eva Šváchová", img: "images/DSC_0213.jpg", text: "Lorem ipsum..." }
+]
+
+// Teachers data
+const it4bTeachers = [
+  { name: "Ing. Lenka Zedníková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Zedníková", subject: "Třídní učitelka, Aplikovaný software v praxi" },
+  { name: "Mgr. Petr Breit", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Breit", subject: "Tvorba webových stránek" },
+  { name: "Mgr. Jan Bezděka, Ph.D.", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Bezděka", subject: "Počítačové sítě" },
+  { name: "Jakub Kovářík", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Kovářík", subject: "Algoritmizace a programování" },
+  { name: "Mgr. Zuzana Kadlecová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Kadlecová", subject: "Anglický jazyk" },
+  { name: "Mgr. Renata Hašková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Hašková", subject: "Anglický jazyk" },
+  { name: "Mgr. Veronika Schejbalová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Schejbalová", subject: "Český jazyk a literatura" },
+  { name: "Mgr. Kateřina Bláhová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Bláhová", subject: "Ekonomika" },
+  { name: "Mgr. Šárka Kůsová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Kůsová", subject: "Matematika" },
+  { name: "Ing. Martin Zábrodský", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Zábrodský", subject: "Operační systémy" },
+  { name: "Mgr. Michal Šilhánek", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Šilhánek", subject: "Tělesná výchova" },
+  { name: "Ing. Jiří Formánek", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Formánek", subject: "Hardware" }
 ];
 
-// Split students into IT4B (boys) and 4SCB (girls) based on name inference with exceptions
-const it4bStudents = students.filter(student => {
-  const fullName = student.name;
-  const firstName = fullName.split(' ')[0];
-  return fullName === "Daryna Toporovska" || [
-    'Daniel', 'Borislav', 'Martin', 'Gabriel', 'Marat', 'Vítek', 'Ivan',
-    'Alexander', 'Jan', 'Jakub', 'Adam', 'David', 'Šimon', 'Tobiáš',
-    'Vojtěch', 'Vaclav'
-  ].includes(firstName);
-});
+const scbTeachers = [
+  { name: "Jana Lechmanová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Lechmanová", subject: "Anglický jazyk" },
+  { name: "Jana Šváchová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Šváchová", subject: "Český jazyk a literatura" },
+  { name: "Helena Klajdáčová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Klajdáčová", subject: "Matematika" },
+  { name: "Iva Vydrová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Vydrová", subject: "Občanská nauka" },
+  { name: "Eva Sevelková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Sevelková", subject: "Praxe, Sociální péče" },
+  { name: "Jana Žižková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Žižková", subject: "Pečovatelství" },
+  { name: "Adéla Brunnerová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Brunnerová", subject: "Pedagogika" },
+  { name: "Lucie Bartovská", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Bartovská", subject: "Psychologie" },
+  { name: "Lenka Zemanová Lindová", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Zemanová Lindová", subject: "Právo" },
+  { name: "Květoslava Mužíková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Mužíková", subject: "Zdravotní tělesná výchova" },
+  { name: "Denisa Kabourková", img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg", alt: "Kabourková", subject: "Tělesná výchova" }
+];
 
-const scbStudents = students.filter(student => {
-  const fullName = student.name;
-  const firstName = fullName.split(' ')[0];
-  return fullName === "Nguyen Quang Truong" || [
-    'Annie', 'Natálie', 'Julie', 'Kristýna', 'Aneta', 'Anna', 'Hana',
-    'Linda', 'Veronika', 'Adéla', 'Barbora', 'Eva'
-  ].includes(firstName);
-});
-
-// Function to render cards
-function renderCards(containerId, studentList) {
+// Function to render student cards
+function renderCards(containerId, list) {
   const container = document.getElementById(containerId);
   container.innerHTML = '';
-  studentList.forEach((student, i) => {
-    const next = (i + 1) % studentList.length;
-    const prev = (i - 1 + studentList.length) % studentList.length;
+  list.forEach((item, i) => {
+    const next = (i + 1) % list.length;
+    const prev = (i - 1 + list.length) % list.length;
     container.innerHTML += `
       <input type="radio" id="radio-${containerId}-${i + 1}" name="radio-card-${containerId}" ${i === 0 ? "checked" : ""}>
       <article class="card" style="--angle:${(Math.random() * 40 - 20).toFixed(1)}deg">
-        <img class="card-img" src="${student.img}" alt="${student.name}">
+        <img class="card-img" src="${item.img}" alt="${item.name}">
         <div class="card-data">
-          <span class="card-num">${i + 1}/${studentList.length}</span>
-          <h2>${student.name}</h2>
-          <p>${student.text}</p>
+          <span class="card-num">${i + 1}/${list.length}</span>
+          <h2>${item.name}</h2>
+          <p>${item.text}</p>
           <footer>
             <label for="radio-${containerId}-${prev + 1}" aria-label="Previous">&#10094;</label>
             <label for="radio-${containerId}-${next + 1}" aria-label="Next">&#10095;</label>
@@ -183,12 +200,29 @@ function renderCards(containerId, studentList) {
   });
 }
 
-// Render cards for both classes
-document.addEventListener("DOMContentLoaded", () => {
-  renderCards("cards-container-it4b", it4bStudents);
-  renderCards("cards-container-scb", scbStudents);
+// Function to render teacher cards in grid
+function renderTeacherCards(containerId, list) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = '';
+  list.forEach(item => {
+    container.innerHTML += `
+      <div class="teacher-card">
+        <img src="${item.img}" alt="${item.alt}">
+        <h3>${item.name}</h3>
+        <p>${item.subject}</p>
+      </div>
+    `;
+  });
+}
 
-  // Swipe functionality for IT4B
+// Render cards for students and teachers
+document.addEventListener("DOMContentLoaded", () => {
+  renderCards("cards-container-it4b", IT4Bstudents);
+  renderCards("cards-container-scb", SCBstudents);
+  renderTeacherCards("teachers-container-it4b", it4bTeachers);
+  renderTeacherCards("teachers-container-scb", scbTeachers);
+
+  // Swipe functionality for IT4B students
   const cardsContainerIt4b = document.querySelector('#cards-container-it4b');
   let startXIt4b = 0;
   cardsContainerIt4b.addEventListener('touchstart', e => startXIt4b = e.touches[0].clientX);
@@ -197,12 +231,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (Math.abs(diff) > 50) {
       const current = document.querySelector('input[name="radio-card-cards-container-it4b"]:checked');
       const currentIndex = [...document.querySelectorAll('input[name="radio-card-cards-container-it4b"]')].indexOf(current);
-      const nextIndex = (currentIndex + (diff < 0 ? 1 : -1) + it4bStudents.length) % it4bStudents.length;
+      const nextIndex = (currentIndex + (diff < 0 ? 1 : -1) + IT4Bstudents.length) % IT4Bstudents.length;
       document.getElementById(`radio-cards-container-it4b-${nextIndex + 1}`).checked = true;
     }
   });
 
-  // Swipe functionality for 4SCB
+  // Swipe functionality for 4SCB students
   const cardsContainerScb = document.querySelector('#cards-container-scb');
   let startXScb = 0;
   cardsContainerScb.addEventListener('touchstart', e => startXScb = e.touches[0].clientX);
@@ -211,7 +245,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (Math.abs(diff) > 50) {
       const current = document.querySelector('input[name="radio-card-cards-container-scb"]:checked');
       const currentIndex = [...document.querySelectorAll('input[name="radio-card-cards-container-scb"]')].indexOf(current);
-      const nextIndex = (currentIndex + (diff < 0 ? 1 : -1) + scbStudents.length) % scbStudents.length;
+      const nextIndex = (currentIndex + (diff < 0 ? 1 : -1) + SCBstudents.length) % SCBstudents.length;
       document.getElementById(`radio-cards-container-scb-${nextIndex + 1}`).checked = true;
     }
   });
